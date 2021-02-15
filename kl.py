7c356635e04e321d48b471d17eaa1eaf5c0c978a
Baburaj R <clickmebaburaj@gmail.com>
2021-02-15 19:45:00 +0530

    updated

--- kl.py
+++ kl.py
@@ -5,7 +5,7 @@ import requests
 from plotly.offline import iplot
 import plotly.graph_objs as go
 import plotly.express as px
-from pandas.io.json import json_normalize
+# from pandas.io.json import json_normalize
 
 
 fig = go.Figure()
@@ -18,5 +18,19 @@ st.write('a simple tracking app for kerala')
 
 url = 'https://keralastats.coronasafe.live/summary.json'
 r = requests.get(url)
-df1 = pd.read_json(r.json)
-st.write(pd.DataFrame(df1))
+df = pd.read_json(r.text)
+last_updated = df['last_updated'][0]
+st.write('Last Updated on: ' + last_updated )
+df = df.drop('last_updated', axis=1)
+# st.write(pd.DataFrame(df))
+df1 = df.drop('delta', axis=1)
+df1 = df1.drop(['hospital_obs','home_obs','total_obs','hospital_today'], axis=0)
+st.bar_chart(df1)
+
+# r2= requests.get('https://keralastats.coronasafe.live/histories.json')
+# df2 = pd.read_json(r2.text)
+# history = df['histories']
+
+# for hist in history:
+#     df_new = pd.DataFrame(hist['summary']).T
+# st.write(pd.DataFrame(df_new))
